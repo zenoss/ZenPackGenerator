b999e61e33ccf719119496aebe67e674acd8f199
Eric Edgar <eedgar@zenoss.com>
2013-05-16 22:54:01 -0500

    File cleanups

--- src/zpg/main.py
+++ src/zpg/main.py
@@ -10,35 +10,6 @@ from lib.ZenPack import ZenPack
 logging.basicConfig()
 log = logging.getLogger('ZenPack Generator')
 
-#def main():
-#    log.info("Starting...")
-
-#    parser = ZpgOptionParser()
-#    (opts, args) = parser.parse_args()
-
-
-
-
-    #pluginMgr = PluginMgr(opts.version)
-    #pluginMgr.getPlugins()
-    #config = pydata.config
-
-    #for plugin in [p for p in pluginMgr.plugins if p.type == 'filter']:
-    #    p = plugin(config,opts)
-    #    config = p.run()
-    #for plugin in [p for p in pluginMgr.plugins if p.type == 'layout']:
-    #    p = plugin(config,opts)
-    #    p.run()
-#
-#    for plugin in pluginMgr.plugins:
-#        if plugin.type in ['layout', 'filter']:
-#            continue
-#        p = plugin(config,opts)
-#        p.run()
-
-#if __name__ == '__main__':
-    #main()
-
 def main():
     log.info('ZenPack Generator Starting')
     parser = ZpgOptionParser()
