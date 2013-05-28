16f10e8d1791c210945ce1645b864aecef53154c
Eric Edgar <eedgar@zenoss.com>
2013-05-28 12:04:30 -0500

    Additional zenpack tests.

--- src/zpg/lib/test/test_ZenPack.py
+++ src/zpg/lib/test/test_ZenPack.py
@@ -1,10 +1,13 @@
 import unittest
+import os
 from zpg.lib.ZenPack import ZenPack
 from zpg.lib.DeviceClass import DeviceClass
 from zpg.lib.Component import Component
 from zpg.lib.License import License
 from zpg.lib.Defaults import Defaults
+from mock import mock_open, patch, MagicMock
 defaults = Defaults()
+
 
 class SimpleSetup(unittest.TestCase):
     def setUp(self):
@@ -12,6 +15,33 @@ class SimpleSetup(unittest.TestCase):
 
     def tearDown(self):
         del(self.zp)
+
+
+class WriteBase(unittest.TestCase):
+    def setUp(self):
+        from zpg.lib.ZenPack import ZenPack
+        self.zp = ZenPack('a.b.c')
+        self.mkdir = os.mkdir
+        self.makedirs = os.makedirs
+
+        os.mkdir = MagicMock(return_value=True)
+        os.makedirs = MagicMock(return_value=True)
+
+    def write(self, obj):
+        m = mock_open()
+        with patch('__builtin__.open', mock_open(read_data='Dummy Data'), create=True) as m:
+            obj.write()
+
+            # Write File Handle
+            wfh = m.return_value.__enter__.return_value
+            self.results = wfh.write.call_args_list
+
+    def tearDown(self):
+        print "Calling teardown"
+        os.mkdir = self.mkdir
+        os.makedirs = self.makedirs
+        del(self.zp)
+
 
 class TestZenPackInitialize(unittest.TestCase):
     #@unittest.skip("Skipping")
@@ -73,6 +103,7 @@ class TestZenPackRelationships(SimpleSetup):
         self.zp.addRelation('Device', 'SystemNodes')
         self.zp.addRelation('Device', 'ClusterPeers')
 
+
 class TestzProperties(SimpleSetup):
     def test_quotes(self):
         self.zp.addZProperty('foo', default='\'')
@@ -83,6 +114,23 @@ class TestzProperties(SimpleSetup):
 
         self.zp.addZProperty('baz', default='baz')
         self.assertEqual(self.zp.zproperties['baz'][1],'\'baz\'')
+
+
+class TestZenPack(SimpleSetup):
+    def testRepr(self):
+        self.assertEqual(repr(self.zp), 'a.b.c \n\tAUTHOR: ZenossLabs <labs@zenoss.com>\n\tVERSION: 0.0.1\n\tLICENSE: gpl')
+
+class TestWriteZenPack(WriteBase):
+
+    def MyUpdateGitTemplatesMock(self):
+        pass
+
+    def testWrite(self):
+        self.zp.updateGitTemplates = self.MyUpdateGitTemplatesMock
+        dc = self.zp.addDeviceClass('Devices/Storage')
+        dc.addComponentType('Component')
+        self.write(self.zp)
+
 
 if __name__ == '__main__':
         unittest.main()
