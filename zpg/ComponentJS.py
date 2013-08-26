a0653e34528fa9940bf95d2f8094b602bf212250
Eric Edgar <eedgar@zenoss.com>
2013-08-26 07:21:16 -0700

    Fix issue where an extra component is being added to the filter.

--- zpg/ComponentJS.py
+++ zpg/ComponentJS.py
@@ -23,8 +23,9 @@ class ComponentJS(Template):
         self.deviceClass = deviceClass
         self.zenpack = self.deviceClass.zenpack
         self.zPythonClass = self.deviceClass.zPythonClass
-        self.ConfigureComponent = "%s.%s" % (
-            self.zPythonClass, self.zPythonClass.split('.')[-1])
+        self.ConfigureComponent = self.zPythonClass
+        #self.ConfigureComponent = "%s.%s" % (
+        #    self.zPythonClass, self.zPythonClass.split('.')[-1])
 
         self.source_template = 'component.js.tmpl'
         self.dest_file = "%s/resources/js/%s.js" % (
